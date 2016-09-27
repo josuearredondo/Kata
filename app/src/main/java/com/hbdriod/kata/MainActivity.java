0e8da75729258b9a3843fc84e8194badf56ed92d
josuearredondo <josue.arredondo@hotmail.fr>
2016-09-27 13:48:16 +0200

    Changed strings ressources, added controls and begin design

--- app/src/main/java/com/hbdriod/kata/MainActivity.java
+++ app/src/main/java/com/hbdriod/kata/MainActivity.java
@@ -2,6 +2,10 @@ package com.hbdriod.kata;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText editText = (EditText) findViewById(R.id.edit_text);
+        final TextView textView = (TextView) findViewById(R.id.text);
+        textView.setText("Bonjour");
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String string = editText.getText().toString();
+                textView.setText(string);
+            }
+        });
     }
 }
