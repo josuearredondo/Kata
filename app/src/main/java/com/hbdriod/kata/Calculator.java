a9f3fbf94478c1282919df61f878c881f4711cf5
josuearredondo <josue.arredondo@hotmail.fr>
2016-09-27 16:53:02 +0200

    Modified Add method to test two numbers

--- app/src/main/java/com/hbdriod/kata/Calculator.java
+++ app/src/main/java/com/hbdriod/kata/Calculator.java
@@ -1,5 +1,9 @@
 package com.hbdriod.kata;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
 /**
  * Created by Human Booster on 27/09/2016.
  */
@@ -10,7 +14,10 @@ public class Calculator {
         if (numbers.isEmpty()) {
             sum = 0;
         } else {
-            sum = ((!numbers.isEmpty()) && (!numbers.isEmpty() && numbers.length() == 3)) ? Character.getNumericValue(numbers.charAt(0))+Character.getNumericValue(numbers.charAt(2)): Character.getNumericValue(numbers.charAt(0));
+            StringTokenizer tokens = new StringTokenizer(numbers, ",");
+            String number1 = tokens.nextToken();
+            String number2 = tokens.nextToken();
+            sum = Integer.parseInt(number1.toString()) + Integer.parseInt(number2.toString());
         }
         return sum;
     }
